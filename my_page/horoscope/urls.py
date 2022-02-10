b7e4111629ee15ac6eef22341f4b191ae2ae7b9a
teo_v <teodantes@gmail.com>
2022-02-11 01:57:14 +0300

    Add zodiac types and an ability to determine a zodiac sign using month and day numbers
    Now this app has a menu of zodiac type with an actual links to the zodiac signs list. And now an user can find what zodiac sign is actual
    for an entered date (month and day in route).

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 
-# Конвертеры роутов
-# Здесь важен порядок
-# Сначала пытаемся конвертировать роут в целое число, потом - в строку
-# Доступные типы для конвертации в документации Django
 urlpatterns = [
     path('', views.index),
-    # Добавляем еще один роут для того, чтобы обработать URL horoscope/ без названия знака зодиака
-    # views.index - функция, которая будет обрабатывать запрос
-    path('<int:zodiac_sign>/', views.get_zodiac_info_by_number),
-    path('<str:zodiac_sign>/', views.get_zodiac_info, name='horoscope-name')
+    path('<int:zodiac_value>/', views.get_zodiac_info_by_number),
+    path('<zodiac_value>/', views.get_zodiac_type_list, name='zodiac_type_name'),
+    path('<zodiac_value>/', views.get_zodiac_info, name='horoscope_name'),
+    path('<int:month>/<int:day>/', views.determine_zodiac_sign),
+    path('<str:month>/<str:day>/', views.determine_zodiac_sign_error)
 ]
--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,57 +1,180 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
-# Создаем словарь для того, чтобы избавиться от бесконечных elif
-# С помощью него мы будем обращаться к нужному нам знаку зодиака
 zodiac_dict = {
-    'aries': 'Aries: March 21 - April 19',
-    'taurus': 'Taurus: April 20 - May 20',
-    'gemini': 'Gemini: May 21 - June 21'
+    'aries':
+        {'description': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+         'types': 'fire',
+         'day_start': 21,
+         'month_start': 3,
+         'day_finish': 20,
+         'month_finish': 4
+         },
+    'taurus':
+        {'description': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+         'types': 'earth',
+         'day_start': 21,
+         'month_start': 4,
+         'day_finish': 21,
+         'month_finish': 5
+         },
+    'gemini':
+        {'description': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+         'types': 'air',
+         'day_start': 22,
+         'month_start': 5,
+         'day_finish': 21,
+         'month_finish': 6
+         },
+    'cancer':
+        {'description': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+         'types': 'water',
+         'day_start': 22,
+         'month_start': 6,
+         'day_finish': 22,
+         'month_finish': 7
+         },
+    'leo':
+        {'description': 'Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+         'types': 'fire',
+         'day_start': 23,
+         'month_start': 7,
+         'day_finish': 21,
+         'month_finish': 8
+         },
+    'virgo':
+        {'description': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+         'types': 'earth',
+         'day_start': 22,
+         'month_start': 8,
+         'day_finish': 23,
+         'month_finish': 9
+         },
+    'libra':
+        {'description': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+         'types': 'air',
+         'day_start': 24,
+         'month_start': 9,
+         'day_finish': 23,
+         'month_finish': 10
+         },
+    'scorpio':
+        {'description': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+         'types': 'water',
+         'day_start': 24,
+         'month_start': 10,
+         'day_finish': 22,
+         'month_finish': 11
+         },
+    'sagittarius':
+        {'description': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+         'types': 'fire',
+         'day_start': 23,
+         'month_start': 11,
+         'day_finish': 22,
+         'month_finish': 12
+         },
+    'capricorn':
+        {'description': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+         'types': 'earth',
+         'day_start': 23,
+         'month_start': 12,
+         'day_finish': 20,
+         'month_finish': 1
+         },
+    'aquarius':
+        {'description': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+         'types': 'air',
+         'day_start': 21,
+         'month_start': 1,
+         'day_finish': 19,
+         'month_finish': 2
+         },
+    'pisces':
+        {'description': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+         'types': 'water',
+         'day_start': 20,
+         'month_start': 2,
+         'day_finish': 20,
+         'month_finish': 3
+         }
+}
+
+zodiac_types = {
+    'fire': ['aries', 'leo', 'sagittarius'],
+    'earth': ['taurus', 'virgo', 'capricorn'],
+    'air': ['gemini', 'libra', 'aquarius'],
+    'water': ['cancer', 'scorpio', 'pisces']
+}
+
+number_of_days = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
 }
 
 
 def get_zodiac_info(request, zodiac_sign):
-    description = zodiac_dict.get(
-        zodiac_sign)  # Пытаемся получить значение по ключу в уже созданном словаре zodiac_dict
-    if description:  # Если ключ есть в словаре, возвращается соответствующее значение
+    description = zodiac_dict.get(zodiac_sign)['description']
+    if description:
         return HttpResponse(description)
-    else:  # Если нет ключа, возвращается соответствующее сообщение
+    else:
         return HttpResponseNotFound(f'No information about {zodiac_sign}!')
 
 
-# Вызов функции при конвертировании полученного значения в число
-def get_zodiac_info_by_number(request, zodiac_sign: int):
-    zodiacs = list(zodiac_dict)  # Присваиваем переменной список из ключей словаря
-    if zodiac_sign > len(zodiacs):  # Если введенное пользователем число будет больше
-        # длины нашего списка, вернется соответствующее сообщение
-        return HttpResponseNotFound(f'Неправильный порядковый номер знака зодиака - {zodiac_sign}!')
-    name_zodiac = zodiacs[zodiac_sign - 1]  # Если все хорошо, то мы преобразуем полученное число в знак зодиака и
-    # по полученному URL обратимся к нужной нам функции
-    # ---
-    # Чтобы не переписывать во всех местах horoscope, если в my_page.urls мы изменим это название, используем
-    # функцию reverse из модуля django.urls.
-    # В аргументах укажем значение переменной name, которое мы передали функции path в urlpatterns нашего приложения.
-    # А в args списком запишем переменную name_zodiac.
-    redirect_url = reverse('horoscope-name', args=[name_zodiac])
+def get_zodiac_info_by_number(request, zodiac_value: int):
+    zodiacs = list(zodiac_dict)
+    if zodiac_value > len(zodiacs):
+        return HttpResponseNotFound(f'There is no a zodiac sign with number {zodiac_value}!')
+    name_zodiac = zodiacs[zodiac_value - 1]
+    redirect_url = reverse('horoscope_name', args=[name_zodiac])
     return HttpResponseRedirect(redirect_url)
 
 
 def index(request):
-    zodiacs = list(zodiac_dict)
-    # Присваиваем переменной список из ключей словаря
+    types = list(zodiac_types)
     li_elements = ''
-    # Создадим пустую строку для того, чтобы заполнить ее необходимой информацией
-    for sign in zodiacs:
-        # С помощью цикла обойдем все знаки зодиака и добавим их как ссылки в переменную li_elements
-        redirect_path = reverse('horoscope-name', args=[sign])
-        li_elements += f'<li><a href="{redirect_path}">{sign.title()}</a></li>'
-        # Каждый пункт списка превратим в ссылку
-        # title() - метод, с помощью которого можно сделать первый символ строки заглавным
-    response = f'''
-    <ol>
-        {li_elements}
-    </ol>
-    '''
-    # Получивишийся результат обрамляем тегами Unordered List
+    for type in types:
+        redirect_path = reverse('zodiac_type_name', args=[type])
+        li_elements += f'<li><a href="{redirect_path}">{type.title()}</a></li>'
+    response = f'''<ul>{li_elements}</ul>'''
     return HttpResponse(response)
-    # Возвращаем полученный результат
+
+
+def get_zodiac_type_list(request, zodiac_value):
+    type_list = zodiac_types.get(zodiac_value)
+    li_elements = ''
+    if type_list:
+        for sign in type_list:
+            redirect_path = reverse('horoscope_name', args=[sign])
+            li_elements += f'<li><a href="{redirect_path}">{sign.title()}</a></li>'
+    elif zodiac_value in zodiac_dict:
+        return get_zodiac_info(request, zodiac_value)
+    else:
+        return HttpResponseNotFound(f'{zodiac_value} is neither a siqn nor a type!')
+    response = f'''<ul>{li_elements}</ul>'''
+    return HttpResponse(response)
+
+
+def determine_zodiac_sign(request, month, day):
+    if month not in range(1, 13):
+        return HttpResponseNotFound('Please, enter valid values!')
+    if day not in range(1, number_of_days[month] + 1):
+        return HttpResponseNotFound('Please, enter valid values!')
+    for sign, info in zodiac_dict.items():
+        if month == info['month_start'] and day >= info['day_start'] or \
+                month == info['month_finish'] and day <= info['day_finish']:
+            return HttpResponse(f'{sign.title()}!')
+
+
+def determine_zodiac_sign_error(request, month, day):
+    return HttpResponseNotFound('Please, enter valid values!')
