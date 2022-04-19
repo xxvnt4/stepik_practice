ca7b885aab0e46bf476cd75873804b2eeefdd7d4
teo_v <teodantes@gmail.com>
2022-04-20 01:02:08 +0300

    ДОБАВЛЕНИЕ ЗАПИСИ В ПРОЕКТ

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+
--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movie_app'
+    'movie_app',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
