5c4263f14d47764a20c9588053e99d15a5d377e3
teo_v <teodantes@gmail.com>
2022-04-27 22:52:53 +0300

    ВЫВОДИМ QUERYSET ШАБЛОН

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movie_app/all_movies.html', context=context)
