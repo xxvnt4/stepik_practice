5d3e6b0373adf7e4843575db10943e6c5051dab7
teo_v <teodantes@gmail.com>
2022-05-03 22:09:13 +0300

    СОРТИРОВКА QUERYSET, ORDER BY

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
-# Create your models here.
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,13 +1,22 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
+
 from .models import Movie
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by(F('year').desc(nulls_last=True))
+    # В данном случае, для сортировки Movie.objects используем метод order_by(), куда аргументом в виде строки
+    # передаем название колонки, по которой будем сортировать.
+    # Например, Movie.objects.order_by('year'), Movie.objects.order_by('rating'), Movie.objects.order_by('year', 'id').
+    # В обратном порядке -- Movie.objects.order_by('-year'), Movie.objects.order_by('-rating').
+    # Также, есть объект F, который импортируется из модуля django.db.models (см. выше в импортах). С помощью него тоже
+    # можно выполнить сортировку, а также выбрать, где расположить элементы со значением NULL - выше или ниже (см. выше).
     context = {
         'movies': movies
     }
     return render(request, 'movie_app/all_movies.html', context=context)
+
 
 def show_one_movie(request, slug_movie:str):
     # Добавим еще одну функцию, которая будет отображать информацию об одном фильме. Эта функция аргументом принимает
