2afe7ed2ad47f8571a6db7ea3f2163e3fe1ec636
teo_v <teodantes@gmail.com>
2022-05-08 00:19:52 +0300

    DJANGO DEBUG TOOLBAR

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie_app',
-    'django_extensions'
+    'django_extensions',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'movie_project.urls'
--- movie_project/movie_project/urls.py
+++ movie_project/movie_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movie_app.urls'))
+    path('', include('movie_app.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
