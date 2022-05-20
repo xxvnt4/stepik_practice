a75f4510a6f93d84e474069be2b32f2ed3403dbf
teo_v <teodantes@gmail.com>
2022-05-20 15:52:24 +0300

    ПОЛЕ CHOICES

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -4,12 +4,22 @@ from django.utils.text import slugify
 
 
 class Movie(models.Model):
+    EURO = 'EUR'
+    USD = 'USD'
+    RUB = 'RUB'
+    CURRENCY_CHOICES = [
+        (EURO, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles'),
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     # В административной панели мы бы хотели иметь возможность сохранять пустые значения в колонке year.
     # Добавим соответствующий аргумент blank=True.
     budget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
 
     def save(self, *args, **kwargs):
