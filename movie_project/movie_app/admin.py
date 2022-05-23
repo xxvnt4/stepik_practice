0d652265886f37f9b1c2e46d6b2b999930cc6aa7
teo_v <teodantes@gmail.com>
2022-05-24 00:54:47 +0300

    НАСТРОЙКА ФОРМЫ ЭЛЕМЕНТА

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -3,15 +3,10 @@ from .models import Movie
 from django.db.models import QuerySet
 
 class RatingFilter(admin.SimpleListFilter):
-    # Класс для собственных фильтров.
     title = 'Фильтр по рейтингу'
-    # Название фильтра в панели.
     parameter_name = 'rating'
-    # Название параметра в адресной строке.
 
     def lookups(self, request, model_admin):
-        # Переопределим этот метод, где вернем нужные нам пункты в виде списка кортежей, где первый элемент
-        # пойдет в адресную строку, а второй - в панель фильтрации.
         return [
             ('<40', 'Низкий'),
             ('от 40 до 59', 'Средний'),
@@ -20,7 +15,6 @@ class RatingFilter(admin.SimpleListFilter):
         ]
 
     def queryset(self, request, queryset: QuerySet):
-        # Переопределим и этот метод, где опишем всю логику фильтрации.
         if self.value() == '<40':
             return queryset.filter(rating__lt=40)
         if self.value() == 'от 40 до 59':
@@ -31,29 +25,21 @@ class RatingFilter(admin.SimpleListFilter):
             return queryset.filter(rating__gte=80)
 
 @admin.register(Movie)
-# Можем навесить этот декоратор на наш класс - в этом случае, нам не придется в дальнейшем регистрировать наши классы
-# отдельно в этом модуле.
 class MovieAdmin(admin.ModelAdmin):
-    # Для организации и удобного управления базой данных в административной панели, создаем новый класс MovieAdmin,
-    # наследуемый от admin.ModelAdmin. Здесь можно выставлять необходимые нам параметры вывода базы данных.
+    # fields = ['name', 'rating']
+    # Поля, отображаемые в форме объекта.
+    exclude = ['budget']
+    # Поля, НЕотображаемые в форме объекта.
+    readonly_fields = ['year']
+    # Нередактируемые поля.
+    prepopulated_fields = {'slug': ('name',)}
+    # Вычисляемые поля.
     list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
-    # В этой переменной прописываются названия колонок, которые мы хотим видеть в нашей таблице. Они будут в том же
-    # порядке. После заполнения этой переменной, элементы базы данных перестают быть вида, объявленного в магическом
-    # методе __str__ класса нашей модели.
     list_editable = ['rating', 'currency', 'budget']
-    # Те колонки, которые мы можем редактировать. Стоит учесть, что здесь не может быть первого элемента списка предыдущей
-    # переменной, так как первая колонка является ссылкой на страницу редактирования элемента базы данных.
-    # Если хотим исправить ошибку с тем, что нельзя сохранять пустые значения в некоторых колонках, то нам нужно перейти
-    # в models.py и дополнительным аргументом к нужной колонке добавить blank=True (см. models.py).
-    # ordering = ['-rating', 'name']
-    # С помощью этой переменной мы можем сортировать нашу базу данных.
     list_per_page = 10
-    # Пагинация - то количество элементов, которые мы хотим видеть на одной странице.
     actions = ['set_dollars', 'set_euro']
     search_fields = ['name__startswith', 'rating']
     list_filter = ['name', 'currency', RatingFilter]
-    # Для того, чтобы создать фильтр по уже существующим колонкам, нужно просто добавить переменную list_filter
-    # с названиями в виде списка. Но если мы хотим создать собственный фильтр, нужно создать класс (см. выше по коду).
 
     @admin.display(ordering='rating', description='Status')
     def rating_status(self, mov: Movie):
@@ -78,9 +64,6 @@ class MovieAdmin(admin.ModelAdmin):
             messages.ERROR
         )
 
-
-
 # admin.site.register(Movie)
 # admin.site.register(MovieAdmin)
-# Та самая регистрация моделей, вместо которой мы просто навешиваем декоратор на класс.
 
--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
+# from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
+# Валидаторы, с помощью которых мы указываем на допустимые значения.
 
 
 class Movie(models.Model):
@@ -14,21 +16,20 @@ class Movie(models.Model):
     ]
 
     name = models.CharField(max_length=40)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),
+                                             MaxValueValidator(100)])
     year = models.IntegerField(null=True, blank=True)
-    # В административной панели мы бы хотели иметь возможность сохранять пустые значения в колонке year.
-    # Добавим соответствующий аргумент blank=True.
-    budget = models.IntegerField(default=1000000)
+    budget = models.IntegerField(default=1000000,
+                                 validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Movie, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super(Movie, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
 
     def get_url(self):
         return reverse('movie_detail', args=[self.slug])
-    # Этим методом можно будет создавать ссылку на страницу с информацией о фильме по slug.
